faa652ec802dbce5b51666ee8d20cd64ae9dc83e
Xiabili Xiahilil <info@javaspin.nl>
2020-01-29 09:33:38 +0100

    add Tamil as additional language

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -78,7 +78,8 @@ module.exports = (options) => ({
             output: {
                 groupBy: [
                     { pattern: "./src/main/webapp/i18n/en/*.json", fileName: "./i18n/en.json" },
-                    { pattern: "./src/main/webapp/i18n/zh-cn/*.json", fileName: "./i18n/zh-cn.json" }
+                    { pattern: "./src/main/webapp/i18n/zh-cn/*.json", fileName: "./i18n/zh-cn.json" },
+                    { pattern: "./src/main/webapp/i18n/tr/*.json", fileName: "./i18n/tr.json" }
                     // jhipster-needle-i18n-language-webpack - JHipster will add/remove languages in this array
                 ]
             }
